01ea301d4f5e94068b3327087fdea069f4aabd29
Brian Tom <tombl@uci.edu>
2018-03-29 23:14:16 -0700

    Update var names

--- tests/movement_test/movement_test.ino
+++ tests/movement_test/movement_test.ino
@@ -2,80 +2,80 @@
 // http://tronixstuff.com/2014/11/25/tutorial-l298n-dual-motor-controller-modules-and-arduino/
 
 // Right motor ports
-const int RightMotorLevel = 5;
-const int RightMotorForward = 6;
-const int RightMotorBackward = 7;
+const int RIGHT_MOTOR_LEVEL = 5;
+const int RIGHT_MOTOR_FORWARD = 6;
+const int RIGHT_MOTOR_BACKWARD = 7;
 
 // Left motor ports
-const int LeftMotorForward = 8;
-const int LeftMotorBackward = 9;
-const int LeftMotorLevel = 10;
+const int LEFT_MOTOR_LEVEL = 8;
+const int LEFT_MOTOR_FORWARD = 9;
+const int LEFT_MOTOR_LEVEL = 10;
 
-// Beep port
-const int Beep = 12;
+// BEEPER port
+const int BEEPER = 12;
 
-// Key port
-const int Key = 13;
+// BUTTON port
+const int BUTTON = 13;
 
 // Setup pin modes and initialization values
 void setup()
 {
   // Right motor pin modes
-  pinMode(RightMotorLevel, OUTPUT);
-  pinMode(RightMotorForward, OUTPUT);
-  pinMode(RightMotorBackward, OUTPUT);
+  pinMode(RIGHT_MOTOR_LEVEL, OUTPUT);
+  pinMode(RIGHT_MOTOR_FORWARD, OUTPUT);
+  pinMode(RIGHT_MOTOR_BACKWARD, OUTPUT);
 
   // Left motor pin modes
-  pinMode(LeftMotorLevel, OUTPUT);
-  pinMode(LeftMotorForward, OUTPUT);
-  pinMode(LeftMotorBackward, OUTPUT);
+  pinMode(LEFT_MOTOR_LEVEL, OUTPUT);
+  pinMode(LEFT_MOTOR_LEVEL, OUTPUT);
+  pinMode(LEFT_MOTOR_FORWARD, OUTPUT);
 
-  // Beep sound pin mode
+  // BEEPER sound pin mode
   // mute = LOW
   // alarm = HIGH
-  pinMode(Beep, OUTPUT); // digital
-  digitalWrite(Beep, LOW);
+  pinMode(BEEPER, OUTPUT); // digital
+  digitalWrite(BEEPER, LOW);
 
-  // Key press (KEY1) pin mode
+  // BUTTON press (BUTTON1) pin mode
   // pressed = LOW
   // unpressed = HIGH
-  pinMode(Key, INPUT); // digital
-  digitalWrite(Key, HIGH);
+  pinMode(BUTTON, INPUT); // digital
+  digitalWrite(BUTTON, HIGH);
 }
 
 // Beep for the specified amount of time,
-// no delay following beep
-// @param time: time in ms to beep for
+// no delay following BEEPER
+// @param time: time in ms to BEEPER for
 void beep(int time)
 {
-  digitalWrite(Beep, HIGH);
+  digitalWrite(BEEPER, HIGH);
   delay(time);
-  digitalWrite(Beep, LOW);
+  digitalWrite(BEEPER, LOW);
 }
 
 // Beep specified number of times for a duration,
-// with same amount of delay time following each beep
-// @param count: number of times to beep
-// @param time: time in ms to beep and pause for
+// with same amount of delay time following each BEEPER
+// @param count: number of times to BEEPER
+// @param time: time in ms to BEEPER and pause for
 void beeps(int count, int time)
 {
   for (int i = 0; i < count; i++)
   {
-    beep(time);
+    BEEPER(time);
     delay(time);
   }
 }
 
-// Ensures key is pressed before continuing execution
+// Ensures BUTTON is pressed before continuing execution
 void pressToStart()
 {
-  int isPressed = digitalRead(Key);
+  int isPressed = digitalRead(BUTTON);
   while (isPressed == HIGH)
   {
-    isPressed = digitalRead(Key);
+    isPressed = digitalRead(BUTTON);
   }
 
-  beeps(1, 1000);
+  BEEPERs(1, 1000);
 }
 
 // Turn right for the specified amount of time,
@@ -84,13 +84,13 @@ void pressToStart()
 void right(int time)
 {
   // Disable right motor
-  analogWrite(RightMotorLevel, 0);
-  digitalWrite(RightMotorForward, LOW);
-  digitalWrite(RightMotorBackward, LOW);
+  analogWrite(RIGHT_MOTOR_LEVEL, 0);
+  digitalWrite(RIGHT_MOTOR_FORWARD, LOW);
+  digitalWrite(RIGHT_MOTOR_BACKWARD, LOW);
   // Enable left motor forward
-  analogWrite(LeftMotorLevel, 200);
-  digitalWrite(LeftMotorForward, HIGH);
-  digitalWrite(LeftMotorBackward, LOW);
+  analogWrite(LEFT_MOTOR_LEVEL, 200);
+  digitalWrite(LEFT_MOTOR_LEVEL, HIGH);
+  digitalWrite(LEFT_MOTOR_FORWARD, LOW);
   // Delay for time
   delay(time);
 }
@@ -98,13 +98,13 @@ void right(int time)
 void brake()
 {
   // Disable right motor
-  analogWrite(RightMotorLevel, 0);
-  digitalWrite(RightMotorForward, LOW);
-  digitalWrite(RightMotorBackward, LOW);
+  analogWrite(RIGHT_MOTOR_LEVEL, 0);
+  digitalWrite(RIGHT_MOTOR_FORWARD, LOW);
+  digitalWrite(RIGHT_MOTOR_BACKWARD, LOW);
   // Disable left motor
-  analogWrite(LeftMotorLevel, 0);
-  digitalWrite(LeftMotorForward, LOW);
-  digitalWrite(LeftMotorBackward, LOW);
+  analogWrite(LEFT_MOTOR_LEVEL, 0);
+  digitalWrite(LEFT_MOTOR_LEVEL, LOW);
+  digitalWrite(LEFT_MOTOR_FORWARD, LOW);
 }
 
 // Main robot movement loop, runs repeatedly
